c37c1f6353a158c0af0b998aef02d87a1b2c63f3
VaibhavDatarkar <vaibhav@peerlogicsys.com>
2022-08-18 19:05:24 +0530

    First commit

---
+++ src/main/java/com/darshana/demo1722/Demo1722Application.java
@@ -0,0 +1,13 @@
+package com.darshana.demo1722;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1722Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1722Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/darshana/demo1722/Demo1722ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darshana.demo1722;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1722ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
